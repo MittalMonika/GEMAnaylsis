de3f12cdcc0f0a04f3af04de8158cdf997125d82
Monika <monika.mittal@cern.ch>
2023-03-03 21:47:03 +0100

    Visulation for Inactive channel scripts

---
+++ plot.sh
@@ -0,0 +1,2 @@
+scp  Output/PFA_Analyzer_Output/Plot/357851/*.pdf /eos/user/m/mmittal/www/GEMPlots/GEMDPG/Inactive/357851/
+scp /eos/user/m/mmittal/www/GEMPlots/GEMDPG/Inactive/index.php /eos/user/m/mmittal/www/GEMPlots/GEMDPG/Inactive/357851/
---
+++ script.py
@@ -0,0 +1,5 @@
+for j in range(2,4):
+    for i in range(1, 385):
+        v = (int(i/128)*j)+(j)
+        print(j,i,v)
+        
